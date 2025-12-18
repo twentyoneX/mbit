30789dea8c38c289bc83879d8432b900ae9f63a2
Mr. X <ximdotro@gmail.com>
2025-12-18 23:00:22 +0200

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'nostr-v1';
+// Since you are in a repo folder, we use relative paths
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
